d4bfecea47dd8257e41486863139daafc67ccb4f
Amit Kumar Pathak <93859479+Amitpathak1996@users.noreply.github.com>
2022-04-17 19:40:23 +0530

    Add files via upload

---
+++ tvz/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Loginpage from './components/Loginpage';
+
+function App() {
+  return (
+    <div>
+      <section  > 
+
+      <Loginpage/>
+
+      </section>
+    </div>
+  );
+}
+
+export default App;
